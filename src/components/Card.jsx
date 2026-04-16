7a6bcc62f15f41352aa748b0789a8b3605437da0
Diyar <sabirovdiyar58@gmail.com>
2026-04-17 02:04:37 +0500

    Dad's birthday

---
+++ src/components/Card.jsx
@@ -0,0 +1,82 @@
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import "../assets/css/card.css";
+import { Link } from 'react-router-dom'; 
+
+function Card() {
+  const [cardClass, setCardClass] = useState("");
+  const [isCardOpened, setIsCardOpened] = useState(false);
+  const timerRef = useRef(null);
+
+  const toggleCard = () => {
+    if (cardClass === "" || cardClass === "close-half") {
+      setCardClass("open-half");
+      setIsCardOpened(true); 
+      if (timerRef.current) clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        setCardClass("open-fully");
+        timerRef.current = null;
+      }, 1000);
+    } else {
+      setCardClass("close-half");
+      if (timerRef.current) clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        setCardClass("");
+        timerRef.current = null;
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center overflow-clip">
+    <div className="w-[400px]  h-screen flex flex-col items-center justify-center">
+      <motion.div  initial={{ opacity: 0, visibility: "hidden" }}
+          animate={{ opacity: 1, visibility: "visible" }}
+          transition={{duration: 1.2}}>
+      <div id="card" className={`${cardClass}`} onClick={toggleCard}>     
+        <div id="card-inside">
+          <div className="wrap">
+            <p>Туылған күнің құтты болсын ардақты әке</p>
+            <p>43 жасқа толған туған күніңізде денсаулық пен шыдамдылық тілеймін.</p>
+            <p>
+              Сіздің жұмысыңызда да бәрі жақсы болады деп сенемін.
+            </p>
+            <p>
+            Алдағы жылдары маған деген үміттеріңізді ақтауға тырысамын.
+            </p>
+            <p className="signed">Atabek Xaldibaev</p>
+          </div>
+        </div>
+
+        <div id="card-front">
+          <div className="wrap">
+            <h1>Happy Birthday!</h1>
+          </div>
+        </div>
+    </div>
+
+      </motion.div>
+
+    {/* prone to bugs */}
+      {isCardOpened && (
+        <motion.div className="-mt-[3rem]" initial={{ opacity: 0, visibility: "hidden" }}
+        animate={{ opacity: 1, visibility: "visible" }}
+        transition={{duration: 1.2}}> 
+        <Link to ='/cake'>
+        <p className="-mt-[4rem] px-7 py-3 bg-customBlue text-white font-medium text-base rounded-full hover:bg-blue-600">
+          Далее...
+          </p>
+        </Link>
+            
+        </motion.div>
+         
+        )}
+
+    </div>
+    
+    </div>
+    
+  );
+}
+
+export default Card;
